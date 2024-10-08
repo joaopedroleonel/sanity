d5fdaba93d3c9999058d072963bfc7d3edb823d7
joaopedroleonel <joaopsegatto@gmail.com>
2024-10-07 21:26:44 -0300

    commit

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import { joaoType } from "./joaoType";
+
+export const schemaTypes = [joaoType]
---
+++ schemaTypes/joaoType.js
@@ -0,0 +1,29 @@
+import {defineField, defineType} from 'sanity'
+
+export const joaoType = defineType({
+  name: 'usuarios',
+  title: 'Usuarios',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'Nome',
+      type: 'string',
+    }),
+    defineField({
+        name: 'Sobrenome',
+        type: 'string',
+    }),
+    defineField({
+        name: 'Idade',
+        type: 'number',
+    }),
+    defineField({
+        name: 'DataDeNascimento',
+        type: 'datetime',
+    }),
+    defineField({
+      name: 'ReceberPromocoes',
+      type: 'boolean',
+    }),
+  ],
+})
